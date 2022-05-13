ad0358103663c24fe4a08453af54d45e445907c2
Mark Wolgin <wolginmark@gmail.com>
2022-05-13 15:00:54 -0400

    Added autogen

---
+++ src/main/java/com/wolginm/amtrak/schedulegenerator/ScheduleGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.wolginm.amtrak.schedulegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wolginm/amtrak/schedulegenerator/ScheduleGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolginm.amtrak.schedulegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
