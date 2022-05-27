677fbaac25e9ba423d1b6e9a3bc9207f089a2562
Mark Wolgin <wolginmark@gmail.com>
2022-05-27 17:55:39 -0400

    Keystone

--- src/main/java/com/wolginm/amtrak/schedulegenerator/controller/ScheduleController.java
+++ src/main/java/com/wolginm/amtrak/schedulegenerator/controller/ScheduleController.java
@@ -1,5 +1,16 @@
 package com.wolginm.amtrak.schedulegenerator.controller;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.wolginm.amtrak.schedulegenerator.service.ScheduleGeneratorService;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +20,28 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/v1/api/schedule")
 public class ScheduleController {
-    
-    
+
+    private ScheduleGeneratorService scheduleGeneratorService;
+
+    public ScheduleController(ScheduleGeneratorService scheduleGeneratorService) {
+        this.scheduleGeneratorService = scheduleGeneratorService;
+    }
+
+    @GetMapping("keystone")
+    public ResponseEntity<String> getKeystone() throws JsonProcessingException, URISyntaxException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        URI uri = new URI("http://localhost:8080");
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Status", "200");
+        headers.set("Content-Type", "application/json");
+
+        return ResponseEntity
+            .created(uri)
+            .headers(headers)
+            .body(objectMapper
+                .writerWithDefaultPrettyPrinter()
+                .writeValueAsString(this.scheduleGeneratorService.getRoute(94)));
+    }
 
 }
--- src/main/java/com/wolginm/amtrak/schedulegenerator/service/ScheduleGeneratorService.java
+++ src/main/java/com/wolginm/amtrak/schedulegenerator/service/ScheduleGeneratorService.java
@@ -1,5 +1,9 @@
 package com.wolginm.amtrak.schedulegenerator.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.wolginm.amtrak.data.models.consolidated.ConsolidatedRoute;
 import com.wolginm.amtrak.data.service.AmtrakDataService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,4 +12,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class ScheduleGeneratorService {
 
+    private AmtrakDataService amtrakDataService;
+
+    @Autowired
+    public ScheduleGeneratorService(AmtrakDataService amtrakDataService) {
+        this.amtrakDataService = amtrakDataService;
+    }
+
+
+    public List<ConsolidatedRoute> getRoute(int route) {
+        return this.amtrakDataService
+            .getRoutes()
+            .stream()
+            .filter((selected) -> selected.getRoute().getRoute_id() == route)
+            .collect(Collectors.toList());
+    }
 }
